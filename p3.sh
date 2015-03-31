9eff557debfddeedf0086736450888fd590d0106
prithvilalla <prithvilalla@gmail.com>
2015-03-30 20:05:48 -0400

    Update p3.sh

--- p3.sh
+++ p3.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-
-for numNodes in 50 200 400 600 800 1000 
+echo "" > p3-output.txt
+echo "numNodes\t\t\ttxPower\t\t\troutingProtocol\t\t\tdataRate\t\t\tbytesRx\t\t\tbytesTx\t\t\tEfficiency" >> p3-output.txt
+clear
+for numNodes in 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000
 do
-	for txPower in 1 20 50 150 300 500
+	for txPower in 1 2 4 8 10 15 20 25 50 100 200 300 400 500
 	do
-		for routingProtocol in "AODV" "OLSR"
+		for routingProtocol in "olsr" "aodv"
 		do
-			for dataRate in 1
+			for trafficIntensity 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 			do
-				echo "" >> p3_out.txt
-				./waf --run "p3 --numNodes=$numNodes --txPower=$txPower --routingProtocol=$routingProtocol --dataRate=$dataRate" >>p3_out.txt 
+				./waf --run "p3 --numNodes=$numNodes --txPower=$txPower --routingProtocol=$routingProtocol --trafficIntensity=$trafficIntensity"
 			done
 		done
 	done
